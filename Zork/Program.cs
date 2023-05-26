0ce056f97087daf09dd7e350404ce1b54b4ed1e3
Kevin Davis <59934145+Waggler@users.noreply.github.com>
2023-05-26 11:47:21 -0400

    Initialize develop

---
+++ Zork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename;
+            Game game = Game.Load(gameFilename);
+            game.Run();
+        }
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
+    }
+}
